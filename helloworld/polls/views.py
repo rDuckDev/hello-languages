fdbfbe9b2e8984c50faf3f4761c8e0ed70ebbcee
rDuckDev <jonathanhermsen@gmail.com>
2020-10-03 16:10:08 -0500

    refactor: simplify 404 handling of detail

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.http import HttpResponse
 
@@ -13,12 +13,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-    return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
