47a2828493ffb357807042f002b4b917d19279ab
rDuckDev <jonathanhermsen@gmail.com>
2020-10-03 16:07:34 -0500

    feat: add detail template

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
 
 from .models import Question
@@ -12,6 +13,11 @@ def index(request):
 
 
 def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
     return HttpResponse("You're looking at question %s." % question_id)
 
 
