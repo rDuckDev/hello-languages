f01b0d0e6bc7fd30d070b221ea8f5a41988abf14
rDuckDev <jonathanhermsen@gmail.com>
2021-02-11 02:06:32 -0600

    feat: add App router for HelloWorld

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Header from './components/Header';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="container my-3">
+        <Switch>
+          <Route path="/" exact component={HelloWorld} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
