e779454e0b628f81e683332a724834834e0995df
rDuckDev <jonathanhermsen@gmail.com>
2021-02-11 02:05:18 -0600

    feat: create HelloWorld component

---
+++ react-app/src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export default class HelloWorld extends React.Component<any, any> {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World!</h1>
+      </Fragment>
+    );
+  }
+}
