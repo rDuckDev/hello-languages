269130e52e4281289000407b35d8a6e8c73d0db7
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-16 16:36:43 -0600

    feat: add skeleton HTML for 'Hello World' [PHP]

--- php/helloworld/hello-world.php
+++ php/helloworld/hello-world.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="h-100">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1/dist/cosmo/bootstrap.min.css">
+
     <title>Hello World!</title>
 </head>
 
-<body></body>
+<body class="h-100 d-flex align-items-center">
+    <section class="container-fluid text-center">
+
+    </section>
+</body>
 
 </html>
