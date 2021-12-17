aaa19b3ba8d831105d4d9ad39a24f12b22170f84
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-17 09:22:24 -0600

    feat: add sandbox html output in [PHP]

---
+++ php/sandbox/giraffe-academy/01-html-out.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Hello World!</title>
+</head>
+
+<body>
+    <?php
+    echo ("<h1>rDuckDev</h1>");
+    echo ("<hr>");
+    echo ("<p>Welcome to the rDuckDev website.</p>")
+    ?>
+</body>
+
+</html>
