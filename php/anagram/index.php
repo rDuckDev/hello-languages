18e0f26354b1ac9c543403ceb363bc8ad1d55450
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-16 18:16:25 -0600

    feat: add anagram skeleton for [PHP]

--- php/anagram/index.php
+++ php/anagram/index.php
@@ -5,10 +5,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1/dist/cosmo/bootstrap.min.css">
+
     <title>Anagram</title>
 </head>
 
 <body>
+    <header>
+        <h1>Anagram Generator</h1>
+    </header>
+    <form>
+        <label for="word">Enter a word:</label>
+        <input id="word" name="word" type="text" />
+        <button id="submit" name="submit" type="button">Submit</button>
+    </form>
+    <section>
+        <h2>Anagrams of {word}:</h2>
+        <!-- TODO: list all anagrams -->
+    </section>
 </body>
 
 </html>
