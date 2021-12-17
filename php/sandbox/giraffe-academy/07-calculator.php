12c8318d5b66d162d9e9cf03df2a681786b6a22b
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-17 15:51:24 -0600

    feat: add calculator lesson [PHP][skeleton]

--- php/sandbox/giraffe-academy/07-calculator.php
+++ php/sandbox/giraffe-academy/07-calculator.php
@@ -1,14 +1,47 @@
 <!DOCTYPE html>
 <html>
 
+<?php
+# assign each parameter its given value, or a default
+$var_x = isset($_GET["var_x"]) ? $_GET["var_x"] : 0;
+$var_y = isset($_GET["var_y"]) ? $_GET["var_y"] : 0;
+
+# special handling for division by zero
+$quot = $var_y == 0
+    ? "undefined"
+    : $var_x / $var_y
+?>
+
 <head>
     <meta charset="utf-8">
     <title>Calculator</title>
 </head>
 
 <body>
-    <?php
-    ?>
+    <form action="07-calculator.php" method="GET">
+        <fieldset>
+            <legend>Calculator</legend>
+
+            <p>Enter two integer number, X and Y.</p>
+
+            <label for="var_x">X:</label>
+            <input name="var_x" type="number"><br>
+
+            <label for="var_y">Y:</label>
+            <input name="var_y" type="number"><br>
+            <br>
+            <input type="submit" value="Calculate">
+        </fieldset>
+    </form>
+    <section>
+        <hr>
+        <strong>Output</strong><br>
+        <?= $var_x . " + " . $var_y . " = " . ($var_x + $var_y) ?><br>
+        <?= $var_x . " - " . $var_y . " = " . ($var_x - $var_y) ?><br>
+        <?= $var_x . " * " . $var_y . " = " . ($var_x * $var_y) ?><br>
+        <?= $var_x . " / " . $var_y . " = " . $quot ?>
+        <hr>
+    </section>
 </body>
 
 </html>
