1764dd0cf46472a4357f531decfebc1f659b367f
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-17 09:26:51 -0600

    feat: add variables sandbox in [PHP]

---
+++ php/sandbox/giraffe-academy/02-variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Mad Libs (Variables)</title>
+</head>
+
+<body>
+    <?php
+    echo ("There once was a man named George.<br>");
+    echo ("He was 70 years old.<br>");
+    echo ("He really liked the name George.<br>");
+    echo ("But didn't like being 70.");
+    ?>
+</body>
+
+</html>
