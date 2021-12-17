850f719eef998f0d54ff050514f221a06e162600
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-17 09:34:58 -0600

    feat: use variables for mad libs sandbox [PHP]

--- php/sandbox/giraffe-academy/02-variables.php
+++ php/sandbox/giraffe-academy/02-variables.php
@@ -8,10 +8,13 @@
 
 <body>
     <?php
-    echo ("There once was a man named George.<br>");
-    echo ("He was 70 years old.<br>");
-    echo ("He really liked the name George.<br>");
-    echo ("But didn't like being 70.");
+    $character_name = "Otis";
+    $character_age = 28;
+
+    echo ("There once was a man named $character_name.<br>");
+    echo ("He was $character_age years old.<br>");
+    echo ("He really liked the name $character_name.<br>");
+    echo ("But didn't like being $character_age.");
     ?>
 </body>
 
