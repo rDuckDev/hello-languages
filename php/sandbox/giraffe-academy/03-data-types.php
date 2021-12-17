d776a148efb89275445fdf1d48239859aa8fe77e
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-17 10:15:27 -0600

    feat: print example data types [PHP][sandbox]

--- php/sandbox/giraffe-academy/03-data-types.php
+++ php/sandbox/giraffe-academy/03-data-types.php
@@ -8,6 +8,22 @@
 
 <body>
     <?php
+    # strings
+    $str = "To be or not to be.";
+    # integer
+    $int = 35;
+    # float
+    $flt = 35.3;
+    # boolean
+    $bln = false;
+    # null
+    $nil = null;
+
+    echo ("String: $str<br>");
+    echo ("Integer: $int<br>");
+    echo ("Decimal: $flt<br>");
+    echo ("Boolean: " . ($bln ? "true" : "false") . "<br>");
+    echo ("Null: " . (is_null($nil) ? "null" : $nil) . "<br>");
     ?>
 </body>
 
