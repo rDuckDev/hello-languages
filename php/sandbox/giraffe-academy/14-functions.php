6eac86f463f9acc274dada91a8fc02242b7fbd7c
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 10:06:29 -0600

    feat: add functions lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/14-functions.php
+++ php/sandbox/giraffe-academy/14-functions.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
+
+<?php
+
+function print_hello($name = null, $age = null)
+{
+    # assign default values, when necessary
+    $name = is_null($name) ? "User" : $name;
+    $age = is_null($age) ? 0 : $age;
+
+    echo ("Hello $name (age $age)!<br>");
+}
+
+?>
+
 <html>
 
 <head>
@@ -8,7 +22,10 @@
 
 <body>
     <?php
-    echo ("Functions");
+    print_hello();
+    print_hello("Tom");
+    print_hello("Dave", 43);
+    print_hello("Oscar", 25);
     ?>
 </body>
 
