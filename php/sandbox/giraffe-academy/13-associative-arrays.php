d8364f230d8b5c5a95e486649bcb6e3a8375cb84
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 09:38:55 -0600

    feat: add associative arrays skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/13-associative-arrays.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Associative Arrays</title>
+</head>
+
+<body>
+    <?php
+    echo ("Associative Arrays");
+    ?>
+</body>
+
+</html>
