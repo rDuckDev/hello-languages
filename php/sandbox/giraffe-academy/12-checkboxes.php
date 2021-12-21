ded11de61ffcdc5444f9b493989af6e727196d17
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 09:17:53 -0600

    feat: add checkboxes lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/12-checkboxes.php
+++ php/sandbox/giraffe-academy/12-checkboxes.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+
+<?php
+$fruits = isset($_POST["fruits"])
+    ? $_POST["fruits"]
+    : null;
+
+$output_style = is_null($fruits)
+    ? "display:none"
+    : "display:block";
+?>
+
 <html>
 
 <head>
@@ -7,9 +18,31 @@
 </head>
 
 <body>
-    <?php
-    echo ("Checkboxes");
-    ?>
+    <form action="12-checkboxes.php" method="POST">
+        <fieldset>
+            <legend>Favorite Fruits</legend>
+
+            <p>Please submit your favorite fruits</p>
+
+            <input name="fruits[]" type="checkbox" value="apples">Apples<br>
+            <input name="fruits[]" type="checkbox" value="oranges">Oranges<br>
+            <input name="fruits[]" type="checkbox" value="pears">Pears<br><br>
+
+            <input type="submit">
+        </fieldset>
+    </form>
+
+    <section style="<?= $output_style ?>">
+        <hr>
+        <strong>Output</strong><br>
+        <p>Preferred fruits:</p>
+        <?php
+        foreach ($fruits as $fruit_name) {
+            echo ($fruit_name . "<br>");
+        }
+        ?>
+        <hr>
+    </section>
 </body>
 
 </html>
