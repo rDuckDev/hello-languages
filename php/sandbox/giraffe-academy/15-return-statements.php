f7dbcab47437b1c583b60c9c46343186b218f9da
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 10:07:43 -0600

    feat: add return statements skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/15-return-statements.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Return Statements</title>
+</head>
+
+<body>
+    <?php
+    echo ("Return Statements");
+    ?>
+</body>
+
+</html>
