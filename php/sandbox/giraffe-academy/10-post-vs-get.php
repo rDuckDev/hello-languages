f1610075cdcb0e4765c4b70062bd9e370821b8bb
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 08:50:54 -0600

    feat: add post vs get lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/10-post-vs-get.php
+++ php/sandbox/giraffe-academy/10-post-vs-get.php
@@ -7,9 +7,38 @@
 </head>
 
 <body>
-    <?php
-    echo ("POST vs GET");
-    ?>
+    <form action="10-post-vs-get.php" method="GET">
+        <fieldset>
+            <legend>GET Fields</legend>
+
+            <label for="username">Username:</label>
+            <input name="username" type="text">
+
+            <input type="submit">
+        </fieldset>
+    </form>
+
+
+    <form action="10-post-vs-get.php" method="POST">
+        <fieldset>
+            <legend>POST Fields</legend>
+
+            <p>Never enter a real password!</p>
+
+            <label for="password">Password:</label>
+            <input name="password" type="password">
+
+            <input type="submit">
+        </fieldset>
+    </form>
+
+    <section>
+        <hr>
+        <strong>Output</strong><br>
+        Your username is <?= isset($_GET["username"]) ? $_GET["username"] : "" ?><br>
+        Your password is <?= isset($_POST["password"]) ? $_POST["password"] : "" ?>
+        <hr>
+    </section>
 </body>
 
 </html>
