359786b9fd1ad8a79fa7858205afa1c244ce41a7
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 09:03:26 -0600

    feat: add arrays lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/11-arrays.php
+++ php/sandbox/giraffe-academy/11-arrays.php
@@ -8,7 +8,31 @@
 
 <body>
     <?php
-    echo ("Arrays");
+    $names_array = array(
+        "Kevin",
+        "Karen",
+        "Oscar",
+        "Jim"
+    );
+
+    echo ("Values: " . print_r($names_array) . "<br>");
+    echo ("Value (2): $names_array[1] <br>");
+
+    $names_array[1] = "Dwight";
+    echo ("Value (2): $names_array[1] <br>");
+
+    # note that arrays are not (necessarily) homogenous
+    $names_array[1] = 400;
+    echo ("Value (2): $names_array[1] <br>");
+
+    $names_array[4] = "Angela";
+    echo ("Value (5): $names_array[4] <br>");
+
+    $names_array[9] = "Angela";
+    echo ("Value (10): $names_array[9] <br>");
+
+    echo ("Values: " . count($names_array) . "<br>");
+    echo ("Values: " . print_r($names_array));
     ?>
 </body>
 
