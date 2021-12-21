bed79ddb41d168eec2d25775804a6786151d1b8e
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 08:26:01 -0600

    feat: add URL parameters skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/09-url-parameters.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>URL Parameters</title>
+</head>
+
+<body>
+    <?php
+    echo ("URL Parameters");
+    ?>
+</body>
+
+</html>
