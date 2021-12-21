1bd6e53197187b88ff81c8dbccd56d1ac2e91b8d
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-21 09:53:08 -0600

    feat: add associative arrays lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/13-associative-arrays.php
+++ php/sandbox/giraffe-academy/13-associative-arrays.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
+
+<?php
+$student = isset($_POST["student"])
+    ? $_POST["student"]
+    : null;
+
+$output_style = is_null($student)
+    ? "display:none"
+    : "display:block";
+?>
+
 <html>
 
 <head>
@@ -8,8 +19,43 @@
 
 <body>
     <?php
-    echo ("Associative Arrays");
+    $grades = array(
+        "Jim" => "A+",
+        "Pam" => "B-",
+        "Oscar" => "C+",
+    );
+
+    echo (count($grades) . " students took an exam...<br>");
+    foreach ($grades as $name => $grade) {
+        echo ("$name received a grade of $grade.<br>");
+    }
+
+    $grades["Jim"] = "F";
+    $grades["Pam"] = "B+";
+    echo ("<br>After careful review, their grades were changed...<br>");
+    foreach ($grades as $name => $grade) {
+        echo ("$name received a grade of $grade.<br>");
+    }
+    echo ("<br>");
     ?>
+
+    <form action="13-associative-arrays.php" method="POST">
+        <fieldset>
+            <legend>Grade Search</legend>
+
+            <label for="student">Student</label>
+            <input name="student" type="text">
+
+            <input type="submit">
+        </fieldset>
+    </form>
+
+    <section style="<?= $output_style ?>">
+        <hr>
+        <strong>Output</strong><br>
+        <?= "$student received a grade of $grades[$student].<br>" ?>
+        <hr>
+    </section>
 </body>
 
 </html>
