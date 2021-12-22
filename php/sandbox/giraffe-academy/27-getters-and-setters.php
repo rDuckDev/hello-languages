e0799cad0e7a86c99172a82f134baab32acfd748
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 17:30:37 -0600

    feat: add getters and setters lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/27-getters-and-setters.php
+++ php/sandbox/giraffe-academy/27-getters-and-setters.php
@@ -1,4 +1,47 @@
 <!DOCTYPE html>
+
+<?php
+
+class Movie
+{
+    public $title;
+    private $rating;
+
+    function __construct($title, $rating)
+    {
+        $this->title = $title;
+        $this->set_rating($rating);
+    }
+
+    public function get_rating()
+    {
+        return $this->rating;
+    }
+
+    public function set_rating($rating)
+    {
+        $valid_ratings = array("G", "PG", "PG-13", "R", "NR");
+        // all ratings must be uppercase
+        $rating = strtoupper($rating);
+
+        if (in_array($rating, $valid_ratings)) {
+            // assign a valid rating
+            $this->rating = $rating;
+        } else {
+            // otherwise, make the movie unrated
+            $this->set_rating("NR");
+        }
+    }
+}
+
+// create a library of movies
+$library = array(
+    new Movie("Avengers", "PG-13"),
+    new Movie("Harry Potter", "Phoenix") // invalid rating
+);
+
+?>
+
 <html>
 
 <head>
@@ -7,9 +50,14 @@
 </head>
 
 <body>
-    <?php
-    echo ("Getters and Setters");
-    ?>
+    <?php foreach ($library as $movie) { ?>
+        <section>
+            <hr>
+            <h2><?= $movie->title ?></h2>
+            <h4>Rated <?= $movie->get_rating() ?></h4>
+            <hr>
+        </section>
+    <?php } ?>
 </body>
 
 </html>
