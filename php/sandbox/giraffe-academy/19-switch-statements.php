3c2d51fa60026f23c9c540028371f8626ffc77eb
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 11:58:51 -0600

    feat: add switch statements skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/19-switch-statements.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Switch Statements</title>
+</head>
+
+<body>
+    <?php
+    echo ("Switch Statements");
+    ?>
+</body>
+
+</html>
