66fda52d0cb5e96dd413cf78e16a7b77bccc7531
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 12:50:54 -0600

    feat: add for loops lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/21-for-loops.php
+++ php/sandbox/giraffe-academy/21-for-loops.php
@@ -8,7 +8,41 @@
 
 <body>
     <?php
-    echo ("For Loops");
+    $term = 10;
+
+    echo ("<p>Let's count to $term!</p>");
+
+    # count from 1 to 10
+    for ($i = 1; $i <= $term; $i++) {
+        # include halves in the count
+        for ($ii = 0; $ii < 10; $ii += 5) {
+            echo ("$i.$ii<br>");
+        }
+    }
+    echo ("<br>");
+
+    $lucky_numbers = array(2, 7, 13, 21, 26, 50);
+    $count = count($lucky_numbers);
+    echo ("Lucky numbers: ");
+    for ($i = 0; $i < $count; $i++) {
+        $num = $lucky_numbers[$i];
+        $sep = $i == $count - 1
+            ? ""
+            : ", ";
+        echo ("$num" . "$sep");
+    }
+    echo ("<br><br>");
+
+    $names = array(
+        "Kevin",
+        "Karen",
+        "Oscar",
+        "Jim"
+    );
+    echo ("Students:<br>");
+    foreach ($names as $name) {
+        echo ("$name<br>");
+    }
     ?>
 </body>
 
