d91171bb41a7e5fffc078e228d4e29b813cb6b53
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 17:05:29 -0600

    feat: add object functions lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/26-object-functions.php
+++ php/sandbox/giraffe-academy/26-object-functions.php
@@ -1,4 +1,34 @@
 <!DOCTYPE html>
+
+<?php
+
+class Student
+{
+    public $name;
+    public $major;
+    public $gpa;
+
+    function __construct($name, $major, $gpa)
+    {
+        $this->name = $name;
+        $this->major = $major;
+        $this->gpa = $gpa;
+    }
+
+    public function has_honors()
+    {
+        $honors_gpa = 3.5;
+        return $this->gpa >= $honors_gpa;
+    }
+}
+
+$class = array(
+    new Student("Jim", "Business", 2.8),
+    new Student("Pam", "Art", 3.6)
+);
+
+?>
+
 <html>
 
 <head>
@@ -7,8 +37,18 @@
 </head>
 
 <body>
-    <?php echo ("Object Functions") ?>
-
+    <?php foreach ($class as $student) { ?>
+        <section>
+            <hr>
+            <h1><?= $student->name ?></h1>
+            <h4><?= $student->major ?> Major</h4>
+            <p>
+                GPA: <?= $student->gpa ?>
+                <?= $student->has_honors() ? "(honors)" : "" ?>
+            </p>
+            <hr>
+        </section>
+    <?php } ?>
 </body>
 
 </html>
