85841bb4ee37301cfa4c3afe51b65c971e49ad40
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 09:19:11 -0600

    feat: add comparative if statements skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/17-if-statements-comparative.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Comparative If Statements</title>
+</head>
+
+<body>
+    <?php
+    echo ("Comparative If Statements");
+    ?>
+</body>
+
+</html>
