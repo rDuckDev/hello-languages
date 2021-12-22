6a96a8cce0245a122b83f022c9981d73def343e9
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 11:56:20 -0600

    feat: add improved calculator lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/18-calculator-improved.php
+++ php/sandbox/giraffe-academy/18-calculator-improved.php
@@ -1,4 +1,37 @@
 <!DOCTYPE html>
+
+<?php
+# assign each parameter its given value, or a default
+$var_x = isset($_POST["var_x"]) ? $_POST["var_x"] : null;
+$var_y = isset($_POST["var_y"]) ? $_POST["var_y"] : null;
+$op = isset($_POST["op"]) ? $_POST["op"] : null;
+
+# cache whether input was given
+$has_input = (bool) !is_null($var_x)
+    && !is_null($var_y)
+    && !is_null($op);
+
+# calculate the result based on user input
+$res = null;
+if ($op == "+") {
+    $res = $var_x + $var_y;
+} elseif ($op == "-") {
+    $res = $var_x - $var_y;
+} elseif ($op == "*") {
+    $res = $var_x * $var_y;
+} elseif ($op == "/") {
+    # apply special handling for division by zero
+    $res = $var_y == 0
+        ? "undefined"
+        : $var_x / $var_y;
+}
+
+
+$style = ($has_input && !is_null($res))
+    ? "display:block"
+    : "display:none";
+?>
+
 <html>
 
 <head>
@@ -11,13 +44,32 @@
         <fieldset>
             <legend>Calculator</legend>
 
-            <input type="submit">
+            <p>
+                Enter two integer values, <em>X</em> and <em>Y</em>.
+                Then, enter the desired <em>Operator</em>.
+                Finally, click <em>Calculate</em>.
+            </p>
+
+            <label for="var_x">X:</label>
+            <input name="var_x" type="number"><br>
+
+            <label for="op">Op:</label>
+            <input name="op" type="text" length="1"><br>
+
+            <label for="var_y">Y:</label>
+            <input name="var_y" type="number"><br><br>
+
+
+            <input type="submit" value="Calculate">
         </fieldset>
     </form>
 
-    <?php
-    echo ("Calculator Improved");
-    ?>
+    <section style="<?= $style ?>">
+        <hr>
+        <strong>Output</strong><br>
+        <?= "The result of $var_x $op $var_y is $res<br>" ?>
+        <hr>
+    </section>
 </body>
 
 </html>
