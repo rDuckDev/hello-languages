210170e97bbc708522f1d6aeb69865e5c69a6259
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 09:14:41 -0600

    feat: add if statement lesson [PHP][sandbox]

--- php/sandbox/giraffe-academy/16-if-statements.php
+++ php/sandbox/giraffe-academy/16-if-statements.php
@@ -8,7 +8,24 @@
 
 <body>
     <?php
-    echo ("If Statements");
+    # randomly assign two boolean values
+    $is_happy = (bool) rand(0, 1);
+    $has_coffee = (bool) rand(0, 1);
+
+    # print the state of each boolean to contextualize the output
+    echo ("\$is_happy = " . ($is_happy ? "true" : "false") . "<br>");
+    echo ("\$has_coffee = " . ($has_coffee ? "true" : "false") . "<br><br>");
+
+    if ($is_happy) {
+        # cheerfully greet a happy user
+        echo ("Have a wonderful day!");
+    } elseif ($has_coffee) {
+        # cheerfully greet an unhappy user with coffee
+        echo ("Enjoy your coffee!");
+    } else {
+        # cautiously greet an unhappy user without coffee
+        echo ("May your day improve.");
+    }
     ?>
 </body>
 
