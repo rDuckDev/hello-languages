c77f1e9c0391d743a240509f9750377f2637e33e
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 11:26:38 -0600

    feat: add improved calculator skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/18-calculator-improved.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Calculator Improved</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <fieldset>
+            <legend>Calculator</legend>
+
+            <input type="submit">
+        </fieldset>
+    </form>
+
+    <?php
+    echo ("Calculator Improved");
+    ?>
+</body>
+
+</html>
