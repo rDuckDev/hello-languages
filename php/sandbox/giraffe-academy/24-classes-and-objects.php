ed84341981ceea491f7804fa306645a916000e05
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 16:14:41 -0600

    feat: add classes and objects skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/24-classes-and-objects.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Classes and Objects</title>
+</head>
+
+<body>
+    <?php
+    echo ("Classes and Objects");
+    ?>
+</body>
+
+</html>
