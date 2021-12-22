e6aea216c36cc1275121468a5f43bbd45680802b
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-22 17:06:47 -0600

    feat: add getters and setters skeleton [PHP][sandbox]

---
+++ php/sandbox/giraffe-academy/27-getters-and-setters.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Getters and Setters</title>
+</head>
+
+<body>
+    <?php
+    echo ("Getters and Setters");
+    ?>
+</body>
+
+</html>
