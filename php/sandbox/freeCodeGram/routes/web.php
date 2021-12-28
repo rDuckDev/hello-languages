f7e20c91fc30e5c0ab11f42f4c5b100649fa4ee7
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-28 16:00:11 -0600

    feat: add posts controller [freeCodeGram]

--- php/sandbox/freeCodeGram/routes/web.php
+++ php/sandbox/freeCodeGram/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+
+Route::get('/post/create', [App\Http\Controllers\PostsController::class, 'create'])->name('post.create');
+Route::post('/post', [App\Http\Controllers\PostsController::class, 'store'])->name('post.store');
