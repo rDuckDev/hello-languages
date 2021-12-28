7884d521d2e40fbec917382cd27fb11f427202ba
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-28 12:46:29 -0600

    feat: update home layout [freeCodeGram]

--- php/sandbox/freeCodeGram/resources/views/home.blade.php
+++ php/sandbox/freeCodeGram/resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <section class="container">
+        <section class="row my-5">
+            <section class="col-3">
+                <section class="px-5">
+                    <img src="https://picsum.photos/200/200" alt="logo" class="rounded-circle img-fluid">
+                </section>
+            </section>
+            <section class="col-9">
+                <h1>freeCodeCamp</h1>
+                <section class="d-flex my-4">
+                    <section class="me-4"><span class="fw-bold">153</span> posts</section>
+                    <section class="me-4"><span class="fw-bold">23k</span> followers</section>
+                    <section class="me-4"><span class="fw-bold">212</span> following</section>
+                </section>
+                <h2 class="fs-6">freeCodeCamp.org</h2>
+                <p class="my-1">
+                    We're a global community of millions of people learning to code together.<br>
+                    LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/
+                </p>
+                <a href="https://www.freecodecamp.org">www.freecodecamp.org</a>
+            </section>
+        </section>
+        <section class="row my-5">
+            <section class="col-4 p-2"><img src="https://picsum.photos/300/300" alt="image" class="img-fluid w-100"></section>
+            <section class="col-4 p-2"><img src="https://picsum.photos/301/301" alt="image" class="img-fluid w-100"></section>
+            <section class="col-4 p-2"><img src="https://picsum.photos/302/302" alt="image" class="img-fluid w-100"></section>
+        </section>
+    </section>
 @endsection
