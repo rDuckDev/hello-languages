de961bc55086bb9cf86ea6d6bdf7c8dd2e80ea3b
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-29 01:02:58 -0600

    refactor: change route order [freeCodeGram]

--- php/sandbox/freeCodeGram/routes/web.php
+++ php/sandbox/freeCodeGram/routes/web.php
@@ -20,9 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
-Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
+Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 
-Route::get('/post/create', [App\Http\Controllers\PostsController::class, 'create'])->name('post.create');
 Route::post('/post', [App\Http\Controllers\PostsController::class, 'store'])->name('post.store');
+Route::get('/post/create', [App\Http\Controllers\PostsController::class, 'create'])->name('post.create');
 Route::get('/post/{post}', [App\Http\Controllers\PostsController::class, 'show'])->name('post.show');
