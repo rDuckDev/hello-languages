84d56b448a0a1eaf76efdf15fd194f0a61e81c0c
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-28 21:16:03 -0600

    feat: allow user to save posts [freeCodeGram]

--- php/sandbox/freeCodeGram/app/Http/Controllers/PostsController.php
+++ php/sandbox/freeCodeGram/app/Http/Controllers/PostsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts.create');
@@ -13,7 +18,19 @@ class PostsController extends Controller
 
     public function store()
     {
-        // TODO: validate and store post data
-        dd(request()->all());
+        $data = request()->validate([
+            'caption' => ['required'],
+            'image' => ['required', 'image']
+        ]);
+
+        $image_path = request('image')->store('uploads', 'public');
+
+        // posts can only be saved to the authenticated user
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $image_path
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id);
     }
 }
