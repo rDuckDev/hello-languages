94ab18f56a0087d95b3ae20840398665f657d768
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2021-12-29 09:38:56 -0600

    feat: show profile data in posts [freeCodeGram]

--- php/sandbox/freeCodeGram/resources/views/posts/show.blade.php
+++ php/sandbox/freeCodeGram/resources/views/posts/show.blade.php
@@ -7,8 +7,22 @@
                 <img src="/storage/{{ $post->image }}" alt="{{ $post->caption }}" class="img-fluid w-100">
             </section>
             <section class="col-4">
-                <h1 class="h3">{{ $post->user->username }}</h1>
-                <p>{{ $post->caption }}</p>
+                <section class="d-flex align-items-center">
+                    <img src="/storage/{{ $post->user->profile->image }}" alt="logo"
+                        class="w-100 img-fluid rouded-circle me-3" style="max-width:40px;">
+                    <h1 class="h5 fw-bold">
+                        <a href="{{ route('profile.show', $post->user->id) }}" class="text-decoration-none text-reset">
+                            {{ $post->user->username }}
+                        </a>
+                    </h1>
+                </section>
+                <hr>
+                <p>
+                    <a href="{{ route('profile.show', $post->user->id) }}" class="text-decoration-none text-reset fw-bold">
+                        {{ $post->user->username }}
+                    </a>
+                    {{ $post->caption }}
+                </p>
             </section>
         </section>
     </section>
