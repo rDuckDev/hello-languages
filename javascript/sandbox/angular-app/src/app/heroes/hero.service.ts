011c6cad54b284e12c526aea709eae2caf6baab4
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-03-04 15:31:54 -0600

    feat: return heroes from service [Angular]

--- javascript/sandbox/angular-app/src/app/heroes/hero.service.ts
+++ javascript/sandbox/angular-app/src/app/heroes/hero.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HEROES } from '../data/mock-heroes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  getHeroes() {
+    return HEROES;
+  }
 }
