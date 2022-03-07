6e9ab26780a0fb5f6e2b230adb922e7856072e06
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-03-07 07:14:08 -0600

    feat: add dashboard component [Angular]

--- javascript/sandbox/angular-app/src/app/app.module.ts
+++ javascript/sandbox/angular-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
