f85e1aab1d2a130f026021362f06400384020ae3
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-03-07 07:29:02 -0600

    feat: use hero detail routing [Angular]

--- javascript/sandbox/angular-app/src/app/heroes/heroes.component.ts
+++ javascript/sandbox/angular-app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/hero';
-import { MessageService } from '../message.service';
 import { HeroService } from './hero.service';
 
 @Component({
@@ -9,21 +8,12 @@ import { HeroService } from './hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
