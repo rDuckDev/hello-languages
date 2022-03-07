48fedf0c93c87585ba66a9dc97ae2a543503385e
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-03-07 07:50:28 -0600

    fix: get a hero in HeroDetail [Angular]

--- javascript/sandbox/angular-app/src/app/hero-detail/hero-detail.component.ts
+++ javascript/sandbox/angular-app/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../heroes/hero.service';
 import { Hero } from '../interfaces/hero';
 
 @Component({
@@ -9,7 +11,19 @@ import { Hero } from '../interfaces/hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => {
+      this.hero = hero;
+    });
+  }
 }
